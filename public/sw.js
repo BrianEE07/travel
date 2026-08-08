506e79c6adb9c74bbe7574e383fe907f6c330539
BrianEE07 <brian.1555406@gmail.com>
2026-08-08 22:10:28 +0800

    Fix mobile navigation polish

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'wei-travel-v14';
+const VERSION = 'wei-travel-v15';
 const PAGE_CACHE = `${VERSION}-pages`;
 const ASSET_CACHE = `${VERSION}-assets`;
 const CORE = ['/', '/fukuoka-2026/', '/offline/', '/home-travel-map.jpg', '/hero-fukuoka.png', '/briefing-fukuoka.jpg', '/icons/icon-192.png', '/icons/icon-512.png', '/manifest.webmanifest'];
